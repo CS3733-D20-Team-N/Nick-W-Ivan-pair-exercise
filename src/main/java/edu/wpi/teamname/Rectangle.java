da2f4a0a894ff3481b8bad475150f49c2554fcb4
Ivan <ieroshenko@wpi.edu>
2020-04-16 15:21:03 -0400

    added rectagle

---
+++ src/main/java/edu/wpi/teamname/Rectangle.java
@@ -0,0 +1,19 @@
+package edu.wpi.teamname;
+
+public class Rectangle {
+    double len;
+    double width;
+
+    public Rectangle(double len, double width){
+        this.len = len;
+        this.width = width;
+    }
+
+    public double getPerimeter(){
+        return 0;
+    }
+
+    public double getArea(){
+        return 0;
+    }
+}
